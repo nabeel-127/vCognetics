aef3a5f2af4f0b265a59a65d3e7e5a67c0ffb670
nabeelali1207 <nabeel1207ali@gmail.com>
2023-06-12 10:02:27 +0500

    modified

--- Training/4_Python/Q2/q2.py
+++ Training/4_Python/Q2/q2.py
@@ -8,23 +8,34 @@ print("Logical Core count: " + str(psutil.cpu_count()))
 print("Physical Core count:", str(psutil.cpu_count(logical=False)))
 
 with open('/proc/cpuinfo', 'r') as file:
+	cpuinfo = file.read()
 	for line in file:
 		if "model name" in line:
 			print(line.split(':')[1].strip())
 			break
+	# print("L1i cache size:", cpuinfo.split('cache size')[1].split('\n')[0].strip())
+	# print("L1d cache size:", cpuinfo.split('cache size')[2].split('\n')[0].strip())
+	# print("L2 cache size:", cpuinfo.split('cache size')[3].split('\n')[0].strip())
+	# print("L3 cache size:", cpuinfo.split('cache size')[4].split('\n')[0].strip())
+	output = subprocess.check_output("lscpu", shell=True).decode().splitlines()
+	cache_sizes = {}
+	for line in output:
+		cache_type, cache_size = line.split(":")
+		cache_sizes[cache_type.strip()] = cache_size.strip()
+	# print(cache_sizes)
+	print("L1i cache size:", cache_sizes["L1i cache"])
+	print("L1d cache size:", cache_sizes["L1d cache"])
+	print("L2 cache size:", cache_sizes["L2 cache"])
+	print("L3 cache size:", cache_sizes["L3 cache"])
+
+	print("Thread(s) per core: " + cpuinfo.split('siblings')[1].split('\n')[0].strip())
+
 
 print("CPU max. frequency: " + str(int(psutil.cpu_freq().max)) + "MHz")
 print("CPU min. frequency: " + str(int(psutil.cpu_freq().min)) + "MHz")
 print("CPU current frequency: " + str(int(psutil.cpu_freq().current)) + "MHz")
 
-with open('/proc/cpuinfo', 'r') as file:
-    cpuinfo = file.read()
-print("L1i cache size:", cpuinfo.split('cache size')[1].split('\n')[0].strip())
-print("L1d cache size:", cpuinfo.split('cache size')[2].split('\n')[0].strip())
-print("L2 cache size:", cpuinfo.split('cache size')[3].split('\n')[0].strip())
-print("L3 cache size:", cpuinfo.split('cache size')[4].split('\n')[0].strip())
 
-print("Thread(s) per core: " + cpuinfo.split('siblings')[1].split('\n')[0].strip())
 
 print("Distributor ID:", subprocess.check_output("lsb_release -si", shell=True).decode().strip())
 print("Distributor Description:", subprocess.check_output("lsb_release -sd", shell=True).decode().strip())
