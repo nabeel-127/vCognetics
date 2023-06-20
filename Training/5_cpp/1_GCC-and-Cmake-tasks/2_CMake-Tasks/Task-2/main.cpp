b34b4cb0e9c9263cc04df89a938ed33f70aab026
nabeelali1207 <nabeel1207ali@gmail.com>
2023-06-20 05:44:26 +0500

    working task-2 again

--- Training/5_cpp/1_GCC-and-Cmake-tasks/2_CMake-Tasks/Task-2/main.cpp
+++ Training/5_cpp/1_GCC-and-Cmake-tasks/2_CMake-Tasks/Task-2/main.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
-#include <chrono>
-#include "mymath.hpp"
 
 int main()
 {
-	int a = 10, b = 5;
-	double sum = 0, difference = 0;
-
-	auto start = std::chrono::steady_clock::now();
-
-	for (int i = 1; i <= 1000000; i++)
-	{
-		sum += mymath::add(a, b);
-		difference += mymath::subtract(a, b);
-		a++;
-		b++;
-	}
-	std::cout << "Sum: " << sum << std::endl;
-	std::cout << "Difference: " << difference << std::endl;
-
-	auto end = std::chrono::steady_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	std::cout << "Time taken: " << duration << " microseconds" << std::endl;
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
--- Training/5_cpp/1_GCC-and-Cmake-tasks/2_CMake-Tasks/Task-2/mymath.cpp
+++
@@ -1,4 +0,0 @@
-#include "mymath.hpp"
-
-int mymath::add(int a, int b) { return a + b; }
-int mymath::subtract(int a, int b) { return a - b; }
