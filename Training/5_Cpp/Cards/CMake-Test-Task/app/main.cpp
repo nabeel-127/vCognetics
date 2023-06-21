814155c3ba25eea12d7735a5486e774c3de981f9
nabeelali1207 <nabeel1207ali@gmail.com>
2023-06-21 05:47:25 -0700

    modified

---
+++ Training/5_Cpp/Cards/CMake-Test-Task/app/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include "../include/student.h"
+
+int main()
+{
+	student temp;
+
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
--- Training/5_Cpp/Cards/CMake-Test-Task/include/student.h
+++ Training/5_Cpp/Cards/CMake-Test-Task/include/student.h
@@ -1,5 +1,5 @@
-#ifndef _STUDENT_H
-#define _STUDENT_H
+#ifndef STUDENT_H
+#define STUDENT_H
 
 #include <string>
 #include <map>
@@ -12,14 +12,24 @@ private:
 		std::string roll_no;
 		int age;
 		float cgpa;
-	};
+	} current_student;
 
-	std::map<std::string /*subject name*/, int /*marks*/> result;
+	std::map<std::string, int> result;
 
 public:
-	int get_subject_marks(std::string subject);
+	student();
+	~student();
+
 	void set_subject_marks(std::string subject, int marks);
+	int get_subject_marks(std::string subject);
 	void print_all_marks();
+
+	void set_roll_no(std::string _roll_no);
+	std::string get_roll_no();
+	void set_age(float _age);
+	float get_age();
+	void set_cgpa(float _cgpa);
+	float get_cgpa();
 
 	/*
 	Implement your get and set functions here to read/write the roll_no,
