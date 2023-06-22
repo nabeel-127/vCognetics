c4b0f110be2cfb5e6e010fe819ce6e8f93d15b07
nabeelali1207 <nabeel1207ali@gmail.com>
2023-06-22 08:43:54 +0500

    modified advanced cpp contepts tasks

---
+++ Training/5_Cpp/Cards/Advanced-Cpp-Concepts/whitelist.cpp
@@ -0,0 +1,16 @@
+#ifndef WHITELIST_CPPP
+#define WHITELIST_CPP
+
+#include "whitelist.hpp"
+
+void whitelist::add_to_whitelist(std::string, student)
+{
+}
+bool whitelist::is_student_present(std::string)
+{
+}
+student whitelist::*get_student_data(std::string)
+{
+}
+
+#endif
