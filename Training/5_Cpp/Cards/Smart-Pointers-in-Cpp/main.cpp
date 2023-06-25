e6dca36541a9b39abbbc91c92a314921ea724856
nabeelali1207 <nabeel1207ali@gmail.com>
2023-06-25 13:56:14 -0700

    smart pointers task nearly complete

--- Training/5_Cpp/Cards/Smart-Pointers-in-Cpp/main.cpp
+++ Training/5_Cpp/Cards/Smart-Pointers-in-Cpp/main.cpp
@@ -2,16 +2,27 @@
 #include "student.hpp"
 #include "rakurai.hpp"
 
+void print_student_record(std::unique_ptr<student>& _my_student)
+{
+	std::cout << "Name: " << _my_student->get_student_name() << std::endl;
+	std::cout << "Roll No: " << _my_student->get_roll_no() << std::endl;
+	std::cout << "Age: " << _my_student->get_age() << std::endl;
+	std::cout << "CGPA: " << _my_student->get_cgpa() << std::endl;
+	_my_student->print_all_marks();
+}
+
 int main()
 {
 	rakurai::database db;
-	std::shared_ptr<student> my_student = db.get_student_reference_2("Nabeel");
+	// std::shared_ptr<student> my_student = db.get_student_reference_1("Nabeel");
+	std::unique_ptr<student> my_student = db.get_student_reference_2("Ali");
 	if (my_student)
 	{
-		std::cout << "Student Found!" << std::endl;
-		std::cout << "Roll No: " << my_student->get_roll_no() << std::endl;
-		std::cout << "Age: " << my_student->get_age() << std::endl;
-		std::cout << "CGPA: " << my_student->get_cgpa() << std::endl;
+		// std::cout << "Student Found!" << std::endl;
+		// std::cout << "Roll No: " << my_student->get_roll_no() << std::endl;
+		// std::cout << "Age: " << my_student->get_age() << std::endl;
+		// std::cout << "CGPA: " << my_student->get_cgpa() << std::endl;
+		print_student_record(my_student);
 	}
 	else
 	{
