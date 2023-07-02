0779067e1a5881c275dc4bf046bd0ba4c5a210b6
nabeelali1207 <nabeel1207ali@gmail.com>
2023-07-02 16:24:30 +0500

    working unit testing

--- Training/5_Cpp/Cards/Unit-Testing-in-Cpp/gtest.cpp
+++ Training/5_Cpp/Cards/Unit-Testing-in-Cpp/gtest.cpp
@@ -1,4 +1,6 @@
 #include "whattotest.h"
+#include "whitelist.hpp"
+#include "student.hpp"
 #include <gtest/gtest.h>
 
 TEST(SquareRootTest, PositiveNos)
@@ -13,6 +15,35 @@ TEST(SquareRootTest, NegativeNos)
 {
 	ASSERT_EQ(-1.0, squareRoot(-15.0));
 	ASSERT_EQ(-1.0, squareRoot(-0.2));
+}
+
+TEST(MainTest, maintest)
+{
+	student my_students[2], *temp;
+	whitelist whitelisted;
+	my_students[0].set_roll_no("BEE173059");
+	my_students[0].set_age(21);
+	my_students[0].set_cgpa(3.5);
+	my_students[0].set_subject_marks("mathematics", 50);
+	my_students[0].set_subject_marks("physics", 60);
+	my_students[0].set_subject_marks("science", 70);
+	my_students[0].set_subject_marks("history", 80);
+
+	my_students[1].set_roll_no("BEE173060");
+	my_students[1].set_age(21);
+	my_students[1].set_cgpa(3.5);
+	my_students[1].set_subject_marks("mathematics", 55);
+	my_students[1].set_subject_marks("physics", 65);
+	my_students[1].set_subject_marks("science", 75);
+	my_students[1].set_subject_marks("history", 85);
+
+	whitelisted.add_to_whitelist("Nabeel", my_students[0]);
+	whitelisted.add_to_whitelist("Ali", my_students[1]);
+
+	ASSERT_EQ(1, whitelisted.is_student_present("Nabeel"));
+
+	// temp = whitelisted.get_student_data("Ali");
+	// temp->print_all_marks();
 }
 
 int main(int argc, char **argv)
