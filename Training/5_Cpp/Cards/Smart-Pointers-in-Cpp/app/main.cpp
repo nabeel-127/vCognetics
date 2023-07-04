b1d70e73eb34216d3d615378b202a075732a497c
nabeelali1207 <nabeel1207ali@gmail.com>
2023-07-04 10:58:37 +0500

    add print_student_record function to main

--- Training/5_Cpp/Cards/Smart-Pointers-in-Cpp/app/main.cpp
+++ Training/5_Cpp/Cards/Smart-Pointers-in-Cpp/app/main.cpp
@@ -4,19 +4,32 @@
 
 void print_student_record(std::unique_ptr<student> &my_student)
 {
-	std::cout << "Name: " << my_student->get_student_name() << std::endl;
-	std::cout << "Roll No: " << my_student->get_roll_no() << std::endl;
-	std::cout << "Age: " << my_student->get_age() << std::endl;
-	std::cout << "CGPA: " << my_student->get_cgpa() << std::endl;
-	my_student->print_all_marks();
+	if (my_student)
+	{
+		std::cout << "Name: " << my_student->get_student_name() << std::endl;
+		std::cout << "Roll No: " << my_student->get_roll_no() << std::endl;
+		std::cout << "Age: " << my_student->get_age() << std::endl;
+		std::cout << "CGPA: " << my_student->get_cgpa() << std::endl;
+		my_student->print_all_marks();
+	}
+}
+void print_student_record(std::shared_ptr<student> &my_student)
+{
+	if (my_student)
+	{
+		std::cout << "Name: " << my_student->get_student_name() << std::endl;
+		std::cout << "Roll No: " << my_student->get_roll_no() << std::endl;
+		std::cout << "Age: " << my_student->get_age() << std::endl;
+		std::cout << "CGPA: " << my_student->get_cgpa() << std::endl;
+		my_student->print_all_marks();
+	}
 }
 
 int main()
 {
 	rakurai::database db;
-	// std::shared_ptr<student> my_student = db.get_student_reference_1("Nabeel");
-	std::unique_ptr<student> my_student1 = db.get_student_reference_2("Ali");
-	std::unique_ptr<student> my_student2 = db.get_student_reference_2("Nabeel");
+	std::shared_ptr<student> my_student1 = db.get_student_reference_1("Ali");
+	std::shared_ptr<student> my_student2 = db.get_student_reference_1("Nabeel");
 	std::unique_ptr<student> my_student3 = db.get_student_reference_2("Ahmed");
 	std::unique_ptr<student> my_student4 = db.get_student_reference_2("asda");
 
